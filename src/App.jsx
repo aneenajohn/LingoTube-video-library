ae4026668f78fa4b83946f490f6c9d9769b644d9
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-12 21:40:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="app-header">Video Library</h1>
+      <div className="app-body">Put your app body here</div>
+    </div>
+  );
+}
