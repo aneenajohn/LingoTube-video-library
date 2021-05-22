dbaff8bb9433f05c9b48eef1a77fffed52d48056
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-22 23:23:40 +0530

    added playlist

--- src/components/DataLoader.jsx
+++ src/components/DataLoader.jsx
@@ -2,7 +2,11 @@ import axios from "axios";
 import { useEffect } from "react";
 import { BACKEND_URL } from "./BackendUrl";
 import { useData } from "./DataContext/DataContext";
-import { ADD_TO_HISTORY, ADD_TO_LIKED } from "./Utils/constants";
+import {
+  ADD_TO_HISTORY,
+  ADD_TO_LIKED,
+  ADD_TO_PLAYLIST
+} from "./Utils/constants";
 
 export function DataLoader() {
   const { DataDispatch } = useData();
@@ -24,6 +28,22 @@ export function DataLoader() {
         data.liked.map((video) =>
           DataDispatch({ type: ADD_TO_LIKED, payLoad: video })
         );
+      }
+    })();
+
+    (async function getPlaylists() {
+      try {
+        const {
+          data: { success, playlists }
+        } = await axios.get(`${BACKEND_URL}playlist`);
+        console.log("playlist", success, playlists);
+        if (success) {
+          playlists.map((playlist) =>
+            DataDispatch({ type: ADD_TO_PLAYLIST, payLoad: playlist })
+          );
+        }
+      } catch (err) {
+        console.error(err);
       }
     })();
   }, []);
