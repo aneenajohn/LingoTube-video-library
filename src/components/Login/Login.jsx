2409ca64339d486c42a590480aee99a63d74c6b9
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-07-13 16:37:07 +0530

    Feat: added show hide password

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import { Header } from "./LoginHeader";
 import { loginReducer } from "./loginReducer";
 import { useAuth } from "../Context/authProvider";
@@ -7,6 +7,8 @@ import { loginService } from "../ServerCalls/ServerCalls";
 import { useLocation, useNavigate, Link } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "./Login.css";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 
 export function Login() {
   const userInfo = {
@@ -18,7 +20,7 @@ export function Login() {
   const { authDispatch } = useAuth();
   const { state } = useLocation();
   const navigate = useNavigate();
-
+  const [showPassword, setShowPassword] = useState(false);
   function signInHandler(e) {
     console.log(e.target.name);
     console.log(e.target.value);
@@ -83,12 +85,28 @@ export function Login() {
           <div className="form">
             <input
               className="form__input"
-              type="password"
+              type={showPassword ? "text" : "password"}
               name="password"
               value={loginState.password}
               onChange={signInHandler}
               required
             ></input>
+            <div>
+              {loginState.password && (
+                <div
+                  className="setPasswordVisibility"
+                  onClick={() =>
+                    setShowPassword((showPassword) => !showPassword)
+                  }
+                >
+                  {showPassword ? (
+                    <VisibilityOffIcon className="showPassword" />
+                  ) : (
+                    <VisibilityIcon className="showPassword" />
+                  )}
+                </div>
+              )}
+            </div>
             <label className="form__label">password</label>
           </div>
           <div className="btn__container">
